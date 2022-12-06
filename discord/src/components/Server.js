47e82a9fcb0394b0fcd12b1ecd04ff61ba8e3c97
luvie23 <luvierallos@gmail.com>
2022-12-06 19:45:50 +0800

    add discord server component

---
+++ discord/src/components/Server.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Server = (props) => {
+    return (
+        <img className="w-10 h-10 my-5 rounded-full" 
+       alt={props.name} src={props.serverImage}/>
+    )
+}
+
+export default Server
--- discord/src/components/Sidebar.js
+++ discord/src/components/Sidebar.js
@@ -1,6 +1,15 @@
 import React from "react";
-import discordlogo from '../assets/discordlogo.png'
+import Server from "./Server";
 
+import discordlogo from '../assets/discordlogo.png'
+import coolServerImage from '../assets/serverphotos/coolserver.jpg'
+
+let servers = [
+    {
+        name: 'Cool Server',
+        image: coolServerImage
+    }
+]
 
 
 const Sidebar = () => {
@@ -9,6 +18,9 @@ const Sidebar = () => {
     return (
         <div className='h-full w-20 bg-[#202226] flex flex-col justify-start items-center'>
             <img className="w-10 py-5 border-b-2 border-[#2C2D31]" src={discordlogo}/>
+            {servers.map(server => (
+                <Server serverImage={server.image} name={server.name}/>
+            ))}
             
         </div>
     )
